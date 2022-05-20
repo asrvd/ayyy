fc81b64019216b087d6f2ddce116dab2f9d53df0
ashish <ashirvadbhushan@gmail.com>
2022-05-20 15:04:12 +0530

    Add copy to  cb

--- components/IconBar.jsx
+++ components/IconBar.jsx
@@ -2,12 +2,19 @@ import Icon from "./Icon";
 import { config } from "../config";
 import ReactTooltip from 'react-tooltip';
 import { useEffect, useState } from "react";
+import toast, { Toaster } from 'react-hot-toast';
 
 export default function IconBar() {
     const [isMounted, setIsMounted] = useState(false)
     useEffect(() => {
         setIsMounted(true)
     }, [])
+    const handleClick = (e) => {
+        navigator.clipboard.writeText(e.target.closest('[data-tip]').dataset.tip)
+        toast.success("Copied to clipboard!", {
+            duration: 2000,
+        })
+    }
     const icons = config.links.map(
         (icon) => {
             if (icon.type == "url") {
@@ -18,20 +25,29 @@ export default function IconBar() {
                         </a>
                     </div>
                 );
-            } else {
+            } else if (icon.type == "hover") {
                 return (
-                    <div className="text-center items-center cursor-pointer" key={icon.slug}>
-                        <a data-tip={icon.text} key={icon.slug}>
+                    <div className="text-center items-center cursor-pointer" key={icon.slug}> 
+                        <a data-tip={icon.text} key={icon.slug} onClick={handleClick}>
                             <Icon icon={icon.slug} />
                         </a>
                         {isMounted && <ReactTooltip place="top" type="dark" effect="float"/>}
                     </div>
                 )
+            } else {
+                return;
             }
         }
     )
     return (
         <div className="flex flex-wrap w-full h-full gap-5 justify-center items-top">
+            <Toaster 
+                toastOptions={{
+                    style: {
+                        background: `${config.textColor}`
+                    }
+                }}
+            />
             {icons}
         </div>
     )
