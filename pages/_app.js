496234b66511cd2d595cc3d61ed9910578476f36
ashish <68690233+asheeeshh@users.noreply.github.com>
2022-07-03 13:56:51 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://umami-brown-phi.vercel.app/umami.js"
+        data-website-id="4c450443-d56a-4484-b14f-01237f9daef9"
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
