8b7ced82427f73dc7f67693b30967d15cfb985fb
ashish <68690233+asheeeshh@users.noreply.github.com>
2022-07-03 14:05:34 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,6 @@ import Script from 'next/script'
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Script
-        strategy="afterInteractive"
-        src="https://umami-brown-phi.vercel.app/umami.js"
-        data-website-id="4c450443-d56a-4484-b14f-01237f9daef9"
-      />
       <Component {...pageProps} />
     </>
   )
